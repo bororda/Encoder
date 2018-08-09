1ae40055b26ef05b692bb2c8ceafe90a4d2b64bf
bororda <vasilevichviktor@gmail.com>
2018-08-09 21:28:15 +0200

    final tested and working version

--- IronCode/IronCode.ino
+++ IronCode/IronCode.ino
@@ -38,37 +38,39 @@ unsigned long debounce;
 //Display settings
 #include <SevSeg.h>
 SevSeg sevseg;
-#define numDigits 3 //num of segments
-byte digitPins[] = {11, 12, 13};  //left to right
+#define numDigits 3                               //num of segments
+byte digitPins[] = {11, 12, 13};                  //left to right
 byte segmentPins[] = {14, 15, 4, 5, 7, 8, 9, 10}; //a to g + dg
-bool resistorsOnSegments = false; // 'false' means resistors 330 Ohm are on digit pins
+bool resistorsOnSegments = false;                 // 'false' means resistors 330 Ohm are on digit pins
 byte hardwareConfig = COMMON_CATHODE;
 unsigned long lastTimeCheckedTemp = 0;
 //Display settings
 //Iron settings
-#define tin 7 // Пин Датчика температуры IN Analog через LM358N
-#define pinpwm 5// порт нагревательного элемента(через транзистор)PWM
+#define tin 7               // Пин Датчика температуры IN Analog через LM358N
+#define pinpwm 6            // порт нагревательного элемента(через транзистор)PWM
 volatile int tempSet = 270; // установленная температура
 volatile byte flag = false; //флаг для управления отображаемой температуры (tempReal или tempSet)
-int tempMin = 200; // минимальная температура
-int tempMax = 480; // максимальная температура
-int tempReal = 20; // переменная датчика текущей температуры
-int temppwmreal = 0; // текущее значение PWM нагревателя
+int tempMin = 200;          // минимальная температура
+int tempMax = 480;          // максимальная температура
+int tempReal;          // переменная датчика текущей температуры
+int temppwmreal = 0;        // текущее значение PWM нагревателя
 int valueToDisplay = 0;
-unsigned long time = 0;//переменная для хранения времени изменения температуры
+unsigned long time = 0; //переменная для хранения времени изменения температуры
 //Temperature boost
-byte flagTempBoost = false; //флаг управления температурным бустом
-unsigned long timeTempBoost = 20000; //время температурного буста
+byte flagTempBoost = false;             //флаг управления температурным бустом
+unsigned long timeTempBoost = 20000;    //время температурного буста
 unsigned long timeTempBoostStarted = 0; //переменная для хранения времени начала буста
-int tempBoostBackup = 0; //переменная для хранения оригинальной температуры
+int tempBoostBackup = 0;                //переменная для хранения оригинальной температуры
 //Temperature boost
 //Iron settings
-void setup() {
-  Serial.begin (9600);
+void setup()
+{
+  Serial.begin(9600);
   //Encoder Setup
   pinMode(CLK, INPUT);
   pinMode(DT, INPUT);
   pinMode(SW, INPUT_PULLUP);
+  pinMode(pinpwm, OUTPUT);
   // настройка прерывания. Для работы достаточно одного прерывания на CLK!
   attachInterrupt(digitalPinToInterrupt(CLK), encoderTick, CHANGE);
   attachInterrupt(digitalPinToInterrupt(SW), encoderTick, CHANGE);
@@ -79,115 +81,163 @@ void setup() {
   sevseg.setBrightness(70);
   sevseg.blank();
   //Display Setup
+  getTempReal();
 }
-void loop() {
+void loop()
+{
   show();
   checkTemperature();
 }
-void checkTemperature() {
-  if ((millis() - lastTimeCheckedTemp) >= 2000) {
+void checkTemperature()
+{
+  if ((millis() - lastTimeCheckedTemp) >= 2000)
+  {
     //--------Boost check ---------------------
-    if (flagTempBoost && ((millis() - timeTempBoostStarted) >= timeTempBoost)) { // if bost time has expired
-      tempBoostDisable();// Turn off the boost
+    if (flagTempBoost && ((millis() - timeTempBoostStarted) >= timeTempBoost))
+    { // if bost time has expired
+      tempBoostDisable(); // Turn off the boost
       showTargetTemp();
     }
     //--------Boost check ---------------------
     //--------Вычисление текущей темепературы относительно установенной -------------------------------------------
-    if (tempReal < tempSet ) { // Если температура паяльника ниже установленной температуры то:
-      if ((tempSet - tempReal) < 16 & (tempSet - tempReal) > 6 ) temppwmreal = 150; // Проверяем разницу между у становленной температурой и текущей паяльника, и если разница меньше 10 градусов, то понижаем мощность нагрева, убираем инерцию перегрева (шим 0-255)
-      else if ((tempSet - tempReal) < 7 & (tempSet - tempReal) > 3) temppwmreal = 120;// Понижаем мощность нагрева, убираем инерцию перегрева
-      else if ((tempSet - tempReal) < 4 ) temppwmreal = 90;// Понижаем мощность нагрева, убираем инерцию перегрева
-      else temppwmreal = 230; // Иначе Подымаем мощность нагрева на максимум для быстрого нагрева до нужной температуры
+    if (tempReal < tempSet)
+    { // Если температура паяльника ниже установленной температуры то:
+      if ((tempSet - tempReal) < 16 & (tempSet - tempReal) > 6)
+        temppwmreal = 190; // Проверяем разницу между у становленной температурой и текущей паяльника, и если разница меньше 10 градусов, то понижаем мощность нагрева, убираем инерцию перегрева (шим 0-255)
+      else if ((tempSet - tempReal) < 7 & (tempSet - tempReal) > 3)
+        temppwmreal = 150; // Понижаем мощность нагрева, убираем инерцию перегрева
+      else if ((tempSet - tempReal) < 4)
+        temppwmreal = 120; // Понижаем мощность нагрева, убираем инерцию перегрева
+      else
+        temppwmreal = 255; // Иначе Подымаем мощность нагрева на максимум для быстрого нагрева до нужной температуры
     }
-    else temppwmreal = 0;//Иначе (если температура паяльника равняется или выше установленной) Выключаем мощность нагрева, отключаем паяльник
+    else
+      temppwmreal = 0;                //Иначе (если температура паяльника равняется или выше установленной) Выключаем мощность нагрева, отключаем паяльник
     analogWrite(pinpwm, temppwmreal); //Вывод в шим порт (на транзистор) значение мощности
 
-    tempReal = analogRead(tin);// считываем текущую температуру
-    tempReal = map(tempReal, 750, 1023, 20, 500); // нужно вычислить
-    tempReal = constrain(tempReal, 20, 500);
+    getTempReal();
 
     lastTimeCheckedTemp = millis();
   }
 }
-void show() {
-  if (flag) { //Display the target temp for 2 sec after it's been set
-    if (millis() - time <= 2000) {
+
+void getTempReal() {
+  int a = analogRead(tin);                   // считываем текущую температуру
+  tempReal = a;
+  Serial.println(a);
+  tempReal = map(tempReal, 120, 1023, 20, 500); // нужно вычислить
+  tempReal = constrain(tempReal, 20, 500);
+  
+}
+
+void show()
+{
+  if (flag)
+  { //Display the target temp for 2 sec after it's been set
+    if (millis() - time <= 2000)
+    {
       valueToDisplay = tempSet;
     }
-    else if (!(millis() - time <= 2000)) {
+    else if (!(millis() - time <= 2000))
+    {
       valueToDisplay = tempReal;
       flag = false;
     }
   }
-  else if (flagTempBoost && !flag) { //Boost is on and the target temp displaying went off - DISPLAY BOOST COUNTDOWN
+  else if (flagTempBoost && !flag)
+  { //Boost is on and the target temp displaying went off - DISPLAY BOOST COUNTDOWN
     valueToDisplay = (timeTempBoost - (millis() - timeTempBoostStarted)) / 1000;
   }
-  else valueToDisplay = tempReal;//Default real temp to be displayed
+  else
+    valueToDisplay = tempReal; //Default real temp to be displayed
 
   sevseg.setNumber(valueToDisplay, -1);
   sevseg.refreshDisplay();
 }
-void showTargetTemp() { // enables the tempSet to be displayed for 2 secs
+void showTargetTemp()
+{ // enables the tempSet to be displayed for 2 secs
   flag = true;
   time = millis();
 }
 //Iron
-void adjustTemp(int delta) {
-  if ((((tempSet - delta) > tempMin) && delta < 0) || (((tempSet + delta) <= tempMax) && delta > 0)) { //будущая температура в пределах мин и макс
+void adjustTemp(int delta)
+{
+  if ((((tempSet - delta) > tempMin) && delta < 0) || (((tempSet + delta) <= tempMax) && delta > 0))
+  { //будущая температура в пределах мин и макс
     tempSet += delta;
   }
-  if (tempSet < tempMin) tempSet = tempMin;
-  if (tempSet > tempMax) tempSet = tempMax;
+  if (tempSet < tempMin)
+    tempSet = tempMin;
+  if (tempSet > tempMax)
+    tempSet = tempMax;
   showTargetTemp();
 }
-void tempBoostEnable() {
+void tempBoostEnable()
+{
   timeTempBoostStarted = millis();
   tempBoostBackup = tempSet;
   tempSet = tempMax;
   flagTempBoost = true;
   showTargetTemp();
 }
-void tempBoostDisable() {
-  tempSet = tempBoostBackup;//switch to prev temp
+void tempBoostDisable()
+{
+  tempSet = tempBoostBackup; //switch to prev temp
   flagTempBoost = false;
 }
 //Iron
 //Encoder
-void encoderTick() {
-  DT_now = digitalRead(CLK);          // читаем текущее положение CLK
-  SW_state = !digitalRead(SW);        // читаем положение кнопки SW
+void encoderTick()
+{
+  DT_now = digitalRead(CLK);   // читаем текущее положение CLK
+  SW_state = !digitalRead(SW); // читаем положение кнопки SW
   // отработка нажатия кнопки энкодера
-  if (SW_state && millis() - debounce > 200) { //статус кнопки и защита от дребезга
+  if (SW_state && millis() - debounce > 200)
+  { //статус кнопки и защита от дребезга
     debounce = millis();
     encoderClick();
     turn_flag = 0; // чтобы не включился режим удержания при нажатии и повороте
   }
-  if (!SW_state && !turn_flag && millis() - debounce > 1000 && millis() - debounce < 3000) { // кнопку отпустили и от нажатия прошло от 1 до 3 сек.
+  if (!SW_state && !turn_flag && millis() - debounce > 1000 && millis() - debounce < 3000)
+  { // кнопку отпустили и от нажатия прошло от 1 до 3 сек.
     encoderHold();
   }
   // вращение ручки
-  if (DT_now != DT_last) {            // если предыдущее и текущее положение CLK разные, значит был поворот
+  if (DT_now != DT_last)
+  { // если предыдущее и текущее положение CLK разные, значит был поворот
     turn_flag = 1;
-    if (digitalRead(DT) != DT_now) {  // если состояние DT отличается от CLK, значит крутим по часовой стрелке
-      if (SW_state) {          // если кнопка энкодера нажата
+    if (digitalRead(DT) != DT_now)
+    { // если состояние DT отличается от CLK, значит крутим по часовой стрелке
+      if (SW_state)
+      { // если кнопка энкодера нажата
         adjustTemp(tmpExtraIncrement);
-      } else {                  // если кнопка энкодера не нажата
+      }
+      else
+      { // если кнопка энкодера не нажата
         adjustTemp(tmpIncrement);
       }
-    } else {                          // если совпадают, значит против часовой
-      if (SW_state) {          // если кнопка энкодера нажата
+    }
+    else
+    { // если совпадают, значит против часовой
+      if (SW_state)
+      { // если кнопка энкодера нажата
         adjustTemp(-tmpExtraIncrement);
-      } else {                  // если кнопка энкодера не нажата
+      }
+      else
+      { // если кнопка энкодера не нажата
         adjustTemp(-tmpIncrement);
       }
     }
   }
-  DT_last = DT_now;                  // обновить значение для энкодера
+  DT_last = DT_now; // обновить значение для энкодера
 }
-void encoderClick() {
+void encoderClick()
+{
   Serial.println("A stub: no need in click function at the time");
 }
-void encoderHold() {
+void encoderHold()
+{
   tempBoostEnable();
 }
 //Encoder
+
